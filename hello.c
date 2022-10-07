b1f06d2b5f4ce4cef9ecdaf8928bacf6fccc8fb6
allen <Allen0203.Chen@senao.com>
2022-10-07 09:53:24 +0800

    github branch test version2

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 
     printf("now is in the test_version3");
     printf("this is in the branch test_version1");
-     
+    printf("this is in the branch test_version1");
  
     return 0;
 }
