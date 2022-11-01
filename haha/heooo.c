28009804b94d2839e9d21eed2d3eedd1b9d7fac5
allen <Allen0203.Chen@senao.com>
2022-11-01 17:31:15 +0800

    allen_test

---
+++ haha/heooo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>h
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a =256;
+    printf("for the test\n");
+
+    printf("for the test\n");
+    return 0;
+}
